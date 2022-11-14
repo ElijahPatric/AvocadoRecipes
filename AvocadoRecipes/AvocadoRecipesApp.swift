f944b88fa3f0a20e365b1177b0430ac7f4598c69
Elijah Patric <foreverelijah@gmail.com>
2022-11-14 16:23:34 -0600

    Initial Commit

---
+++ AvocadoRecipes/AvocadoRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadoRecipesApp.swift
+//  AvocadoRecipes
+//
+//  Created by Elijah on 11/14/22.
+//
+
+import SwiftUI
+
+@main
+struct AvocadoRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
